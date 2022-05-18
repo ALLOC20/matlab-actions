5a585a80e763d7c089c6c8ded0769dc1e0adb0a5
ALLOC20 <63141816+ALLOC20@users.noreply.github.com>
2022-05-18 18:40:28 +0800

    Update Rayleigh_fading.m

--- scripts/Rayleigh_fading.m
+++ scripts/Rayleigh_fading.m
@@ -32,7 +32,7 @@ for i = 1:length(sigma_)
     set(gca,'GridLineStyle',':','GridColor','k','GridAlpha',1);
     xlabel('R');
     ylabel('PDF');
-    title('Rayleigh衰落的概率密度函数');
+    title('Rayleigh Fading PDF');
     legend('Theory \sigma=0.5','Simula \sigma=0.5','Theory \sigma=1','Simula \sigma=1',...
         'Theory \sigma=2','Simula \sigma=2','Theory \sigma=3','Simula \sigma=3',...
         'Theory \sigma=4','Simula \sigma=4');
